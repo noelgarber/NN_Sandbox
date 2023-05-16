0415058da2437c241c83aa1cf1fb906dc747d30a
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-05-15 23:58:17 -0400

    Update comments to add more detail

--- DAT112 - Introduction to Neural Networks for Graduate Students/strawberry_nn.py
+++ DAT112 - Introduction to Neural Networks for Graduate Students/strawberry_nn.py
@@ -21,24 +21,26 @@ image_height, image_width = x_train.shape[1], x_train.shape[2]
 model = keras.models.Sequential([
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_height, image_width, 3)),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Dropout(0.25),  # Add dropout with a rate of 0.25
+    keras.layers.Dropout(0.25),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Dropout(0.25),  # Add dropout with a rate of 0.25
+    keras.layers.Dropout(0.25),
     keras.layers.Conv2D(128, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Dropout(0.25),  # Add dropout with a rate of 0.25
+    keras.layers.Dropout(0.25),
     keras.layers.Flatten(),
-    keras.layers.Dropout(0.5),  # Add dropout with a rate of 0.5
+    keras.layers.Dropout(0.5),
     keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dropout(0.5),  # Add dropout with a rate of 0.5
+    keras.layers.Dropout(0.5),
     keras.layers.Dense(3, activation='softmax')
 ])
 
-# Compile the model
+# Compile the model:
+#    --> the ADAM optimizer is an adaptive learning rate optimization algorithm
+#    --> the loss function is sparse_categorical_crossentropy, which converts integer categories to one-hot-encoding
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
+# Train the model; 10 epochs appears sufficient for full minimization of the loss function
 model.fit(x_train, y_train, epochs=10, batch_size=32)
 
 # Evaluate the model on training data
